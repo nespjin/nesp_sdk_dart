ee551e66de5cd9bb0697851b53c5bc68436d8d29
jzl <dyjinzl@chint.com>
2023-09-05 14:17:42 +0800

    feat(*): Init library project

--- bin/nesp_sdk_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:nesp_sdk_dart/nesp_sdk_dart.dart' as nesp_sdk_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${nesp_sdk_dart.calculate()}!');
-}
---
+++ example/nesp_sdk_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:nesp_sdk_dart/nesp_sdk_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
