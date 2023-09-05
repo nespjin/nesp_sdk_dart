84f70116655ae0f68fb1f785b72fb6358c631a8d
jzl <dyjinzl@chint.com>
2023-09-05 15:46:25 +0800

    chore(*): Update Copyright

--- lib/src/lang/list.dart
+++ lib/src/lang/list.dart
@@ -1,13 +1,14 @@
 /*
- * Copyright (c) 2023. NESP Technology Corporation. All rights reserved.
+ * Copyright (c) 2023-2023. NESP Technology.
  *
- * This program is not free software; you can't redistribute it and/or modify it
- * without the permit of team manager.
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing.
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License 
+ * for the specific language governing permissions and limitations under the License.
  *
- * If you have any questions or if you find a bug,
- * please contact the author by email or ask for Issues.
  */
 
 
