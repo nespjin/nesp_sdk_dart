6b55f7401a8d6c87e4a86cc7e9b637aab6642d91
Jack Jin <1756404649@qq.com>
2025-06-08 12:44:28 +0800

    style(scripts): add empty lines at the beginning of Python scripts
    - Add two empty lines at the start of config.py, create-package.py, and update-sdk-version.py
    - This change improves code readability and follows Python coding conventions

--- tools/scripts/config.py
+++ tools/scripts/config.py
@@ -2,6 +2,8 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
+
+
 DESCRIPTION = 'The Http library for the NESP Dart software development kit.'
 REPOSITORY = 'https://github.com/nespjin/nesp_sdk_dart'
 SDK_VERSION = '^3.3.0'
--- tools/scripts/update-sdk-version.py
+++ tools/scripts/update-sdk-version.py
@@ -4,6 +4,8 @@
 # BSD-style license that can be found in the LICENSE file.
 
 
+
+
 import logging
 import sys
 import config
