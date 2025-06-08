e84177a75beadd3a9181390266d05d657a091193
Jack Jin <1756404649@qq.com>
2025-06-08 12:30:58 +0800

    refactor: update copyright year and license across multiple files
    - Update copyright year to 2025 in various source files
    - Replace Apache License 2.0 with BSD-style license
    - Remove unnecessary license text and simplify to a single line
    - Update file headers across multiple packages and files

--- packages/nesp_sdk_dart_core/lib/src/exceptions.dart
+++ packages/nesp_sdk_dart_core/lib/src/exceptions.dart
@@ -1,3 +1,7 @@
+// Copyright 2025, the nesp_sdk_dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 class NespException implements Exception {
   const NespException([this.message = '']) : parent = null;
 
